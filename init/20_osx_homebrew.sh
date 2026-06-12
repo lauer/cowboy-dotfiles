0ed2fe0e9e7bf1cd842f2a8ed57d42b6fb907348
Jesper Grann Laursen <jesper.laursen@dstny.dk>
2026-06-12 11:35:46 +0200

    chore(brew): Using brewfile to install deps and vscode plugins

--- init/20_osx_homebrew.sh
+++ init/20_osx_homebrew.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
 # OSX-only stuff. Abort if not OSX.
 is_osx || return 1
 
 # Install Homebrew.
 if [[ ! "$(type -P brew)" ]]; then
   e_header "Installing Homebrew"
-  true | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  true | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Exit if, for some reason, Homebrew is not installed.
@@ -14,26 +15,7 @@ e_header "Updating Homebrew"
 brew doctor
 brew update
 
-# Functions used in subsequent init scripts.
+cd $DOTFILES || exit 33
 
-# Tap Homebrew kegs.
-function brew_tap_kegs() {
-  kegs=($(setdiff "${kegs[*]}" "$(brew tap)"))
-  if (( ${#kegs[@]} > 0 )); then
-    e_header "Tapping Homebrew kegs: ${kegs[*]}"
-    for keg in "${kegs[@]}"; do
-      brew tap $keg
-    done
-  fi
-}
-
-# Install Homebrew recipes.
-function brew_install_recipes() {
-  recipes=($(setdiff "${recipes[*]}" "$(brew list)"))
-  if (( ${#recipes[@]} > 0 )); then
-    e_header "Installing Homebrew recipes: ${recipes[*]}"
-    for recipe in "${recipes[@]}"; do
-      brew install $recipe
-    done
-  fi
-}
+e_header "Installing Homebrew packages"
+brew bundle
